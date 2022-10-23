818e4bb43a1abc76cd3c3d387904a06674868d7f
Frank Staals <frank@fstaals.net>
2022-10-23 21:07:17 +0200

    more work on the adjacency list representation

--- src/Hiraffe/AdjacencyListRep.hs
+++ src/Hiraffe/AdjacencyListRep.hs
@@ -1,56 +1,89 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Hiraffe.AdjacencyListRep
   ( Graph
   ) where
 
 
 import           Control.Lens
+import           Data.Bifoldable
+import           Data.Bifunctor
+import           Data.Bitraversable
+import qualified Data.IntMap as IntMap
 import qualified Data.Map as Map
 import           Data.Monoid
 import qualified Data.Sequence as Seq
+import           GHC.Generics
 import           Hiraffe.Graph
 
 
 --------------------------------------------------------------------------------
 
-newtype Graph v e = Graph (Map.Map v (Seq.Seq (v,e)))
-                  deriving (Show,Eq)
+data Adj e = Adj { vId    :: {-# UNPACK #-}!Int -- = VertexIx v
+                 , eData :: !e
+                 } deriving (Show,Eq,Ord,Generic,Functor,Foldable,Traversable)
+makeLenses ''Adj
 
 
+data VertexData v e = VertexData { _vData :: !v
+                                 , _neighs :: Seq.Seq (Adj e)
+                                 } deriving (Show,Eq,Ord,Generic,Functor,Foldable,Traversable)
+makeLenses ''VertexData
 
+instance Bifunctor VertexData where
+  bimap = bimapDefault
+instance Bifoldable VertexData where
+  bifoldMap = bifoldMapDefault
+instance Bitraversable VertexData where
+  bitraverse f g (VertexData v ns) = VertexData <$> f v <*> traverse (traverse g) ns
 
-
+-- | A graph in which the vertices themselves are indices
+newtype Graph v e = Graph (IntMap.IntMap (VertexData v e))
+                  deriving (Show,Eq,Generic,Functor,Foldable,Traversable)
 
 instance HasVertices' (Graph v e) where
   type Vertex   (Graph v e) = v
   type VertexIx (Graph v e) = Int
 
-  vertexAt i pafb (Graph m) = undefined -- case Map.elemAt i m of
-                                -- Nothing -> pure
-    -- where
-    --   mkv =
+  vertexAt i = vertexDataOf i <. vData
 
-    -- pafb $
+vertexDataOf                  :: VertexIx v
+                              -> IndexedTraversal' (VertexIx (Graph v e))
+                                                   (Graph v e)
+                                                   (VertexData v e)
+vertexDataOf i pafb (Graph m) = Graph <$> iix i pafb m
+
 instance HasVertices (Graph v e) (Graph v' e) where
-  vertices = undefined
+  -- this is going to be expensive....
+  vertices pvfv' (Graph m) = Graph <$> IntMap.traverseWithKey f m
+    where
+      f i vd = vd&vData %%~ indexed pvfv' i
 
 instance HasEdges' (Graph v e) where
   type Edge   (Graph v e) = e
-  type EdgeIx (Graph v e) = (Int,Int) -- lexicographical index
+  type EdgeIx (Graph v e) = (Int,Int) -- = (VertexIx v,VertexIx v) -- lexicographical index
 
+  edgeAt (u,v) = vertexDataOf u <.> edge v
+    where
+      edge v pefe' vd = undefined -- vd&neighs.i
+      -- TODO: store neighs differently, since this way we cannot efficiently find v.
 
 instance HasEdges (Graph v e) (Graph v e') where
-
+  edges pefe' (Graph m) = Graph <$> IntMap.traverseWithKey f m
+    where
+      f u vd = vd&neighs.traverse %%~ \(Adj v e) -> Adj v <$> indexed pefe' (u,v) e
 
 
 instance Ord v => Graph_ (Graph v e) where
-  fromAdjacencyLists = Graph . foldMap (\(v, adjs) -> Map.singleton v (f adjs))
-    where
-      f = foldMap Seq.singleton
+  fromAdjacencyLists = undefined
+
+    -- Graph . foldMap (\(v, adjs) -> Map.singleton v (f adjs))
+    -- where
+    --   f = foldMap Seq.singleton
 
 
 
-  numVertices (Graph m) = Map.size m
-  numEdges    (Graph m) = getSum . foldMap (Sum . length) $ m
+  numVertices (Graph m) = IntMap.size m
+  numEdges    (Graph m) = getSum . foldMap (Sum . undefined) $ m
 
   neighboursOf u = undefined
 
