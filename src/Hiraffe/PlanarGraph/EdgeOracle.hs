7d16198fe5d1bd2c388616b8589be3edd9cc5fc9
Frank Staals <frank@fstaals.net>
2023-07-26 12:44:08 +0200

    with both data

--- src/Hiraffe/PlanarGraph/EdgeOracle.hs
+++ src/Hiraffe/PlanarGraph/EdgeOracle.hs
@@ -21,7 +21,8 @@ module Hiraffe.PlanarGraph.EdgeOracle
 import           Control.Applicative (Alternative(..))
 import           Control.Lens hiding ((.=))
 import           Control.Monad (forM_)
-import           Control.Monad.ST (ST)
+import           Control.Monad.ST (ST, runST)
+import           Data.Coerce
 import qualified Data.Foldable as F
 import           Data.Maybe (catMaybes, isJust)
 import qualified Data.Vector as V
@@ -41,16 +42,19 @@ import           Hiraffe.PlanarGraph.Dart
 -- either in u's adjacency list or in v's. This can be done s.t. all adjacency
 -- lists have length at most 6.
 --
+-- pre: the edges are considerd undirectional; i.e. the data stored at (u,v) is supposed
+-- to be the same as at (v,u). find may arbitrarily return only one of the two values.
+--
 -- note: Every edge is stored exactly once (i.e. either at u or at v, but not both)
 newtype EdgeOracle s w e = EdgeOracle (V.Vector (V.Vector (VertexIdIn w s :+ e)))
                          deriving (Show,Eq,Functor,Foldable,Traversable)
 
--- data DartData e = UToVOnly e
---                 | VToUOnlly e
---                 | Both { uToV :: e -- ^ the data associated with the dart in this direction
---                        , vToU :: e -- ^ the data associated with the reverse direction
---                        }
---                 deriving (Show,Eq,Functor,Foldable,Traversable)
+data DartData e = UToV e
+                | VToU e
+                | Both { uToV :: e -- ^ the data associated with the dart in this direction
+                       , vToU :: e -- ^ the data associated with the reverse direction
+                       }
+                deriving (Show,Eq,Functor,Foldable,Traversable)
 
 -- | Given a planar graph, construct an edge oracle. Given a pair of vertices
 -- this allows us to efficiently find the dart representing this edge in the
@@ -66,6 +70,39 @@ edgeOracle g = buildEdgeOracle [ (v, mkAdjacency v <$> incidentEdges v g)
   where
     mkAdjacency v d = otherVtx v d :+ d
     otherVtx v d = let u = tailOf d g in if u == v then headOf d g else u
+
+
+withBothData              :: forall s w e. V.Vector (V.Vector (VertexIdIn w s :+ e))
+                          -> EdgeOracle s w e
+                          -> EdgeOracle s w (DartData e)
+withBothData inAdj oracle = EdgeOracle oOut
+  where
+    (EdgeOracle os) = UToV <$> oracle
+    -- | Finds the index of v in the adjacencylist of u
+    find'     :: VertexIdIn w s -> VertexIdIn w s -> Maybe Int
+    find' u v = V.findIndex ((== v) . (^.core)) $ os V.! (coerce u)
+
+    oOut = runST $
+           do out <- mapM V.thaw os
+              V.iforM_ inAdj $ \ui adjU -> do
+                let u = VertexId ui
+                forM_ adjU $ \(v :+ eUV) ->
+                  case find' u v of
+                    Nothing -> case find' v u of
+                      Just i  -> assign' out v i eUV -- (u,v) is stored at u
+                      Nothing -> error $
+                        "EdgeOracle.withBothData, edge" <> show (u,v) <> "not stored at all!"
+                    Just _  -> pure () -- (u,v) is stored at v, so eUV is already stored there
+              mapM V.unsafeFreeze out
+
+    assign' out u i eVU = let adjU = out V.! (coerce u)
+                          in MV.modify adjU (fmap (combine eVU)) i
+
+    combine eVU = \case
+      UToV eUV   -> Both eUV eVU
+      VToU _     -> VToU eVU -- apparently we are updating VToU
+      Both eUV _ -> Both eUV eVU
+      -- I don't think these last two cases should occur.
 
 
 -- | Builds an edge oracle that can be used to efficiently test if two vertices
@@ -86,6 +123,7 @@ buildEdgeOracle inAdj' = EdgeOracle $ V.create $ do
     -- the adjacencies of unprocessed vertices.
   where
     -- Convert to a vector representation
+    inAdj :: V.Vector (V.Vector (VertexIdIn w s :+ e))
     inAdj = V.create $ do
               mv <- MV.new (length inAdj')
               forM_ inAdj' $ \(VertexId i,adjI) ->
@@ -102,8 +140,6 @@ buildEdgeOracle inAdj' = EdgeOracle $ V.create $ do
                        -> ST s' (V.Vector (VertexIdIn w s :+ e))
     extractAdj marks i = let p = fmap not . UMV.read marks . (^.core.unVertexId)
                          in GV.filterM  p $ inAdj V.! i
-      -- seems that here we should make sure to remember the data in the other direction
-      -- as well
 
     -- | Decreases the number of adjacencies that vertex j has
     -- if it has <= 6 adjacencies left it has become available for processing
