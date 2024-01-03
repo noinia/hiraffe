47f470599d7f9aec507d9978b2a248b1722b1bc5
Frank Staals <frank@fstaals.net>
2024-01-03 21:33:21 +0100

    bfs testcases geupdate

--- src/Hiraffe/Tree.hs
+++ src/Hiraffe/Tree.hs
@@ -8,6 +8,7 @@
 module Hiraffe.Tree
   ( pathsTo
   , findPaths
+  , labelPrefixWith
   ) where
 
 import           Data.List.NonEmpty (NonEmpty(..))
@@ -29,3 +30,27 @@ findPaths p = go
                               | otherwise -> []
                         paths | p x       -> (x:|[]) : map (x NonEmpty.<|) paths
                               | otherwise ->           map (x NonEmpty.<|) paths
+
+-- -- |
+-- foldTree   :: (a -> [r] -> r) -> Tree a -> r
+-- foldTree f = go
+--   where
+--     go (Node x chs) = f x (map go chs)
+
+-- -- distances :: Tree a -> [(a,Int)]
+-- -- distances = mconcat . zipWith (\d lvl -> (,d) <$> lvl) [0..] . levels
+
+-- -- | Label all nodes with their distances from the root.
+-- labelWithDistances :: Integral i => Tree a -> Tree (a, i)
+-- labelWithDistances = labelPrefixWith 0 (const (1 + ))
+
+
+
+-- | Label all nodes with their "distances" from the root.
+--
+-- >>> labelPrefixWith 0 (const succ) $ Node "r" [Node "a" [], Node "b" [Node "foo" []], Node "c" []]
+-- Node {rootLabel = ("r",0), subForest = [Node {rootLabel = ("a",1), subForest = []},Node {rootLabel = ("b",1), subForest = [Node {rootLabel = ("foo",2), subForest = []}]},Node {rootLabel = ("c",1), subForest = []}]}
+labelPrefixWith     :: d -> (a -> d -> d) -> Tree a -> Tree (a, d)
+labelPrefixWith z f = go z
+  where
+    go d (Node x chs) = Node (x,d) [ go (f x d) ch | ch <- chs ]
--- test/Hiraffe/BFSSpec.hs
+++ test/Hiraffe/BFSSpec.hs
@@ -1,15 +1,25 @@
 module Hiraffe.BFSSpec (spec) where
 
-import Data.Graph (Graph,buildG)
-import Data.Tree
-import Hiraffe.BFS
-import Test.Hspec
+import qualified Data.Foldable as F
+import           Data.Graph (Graph,buildG)
+import qualified Data.Map as Map
+import           Data.Tree
+import           Hiraffe.BFS
+import           Hiraffe.Tree
+import           Test.Hspec
 
 --------------------------------------------------------------------------------
 
 spec :: Spec
-spec = it "BFS test" $
-         bfs testGr 0 `shouldBe` answer
+spec = describe "BFS test" $ do
+         it "BFS tree" $
+           bfs testGr 0 `shouldBe` answer
+         it "distances" $
+           let dists = Map.fromList . F.toList . labelWithDistances $ bfs testGr 0
+           in dists `shouldBe` distances
+
+labelWithDistances :: Tree a -> Tree (a, Int)
+labelWithDistances = labelPrefixWith 0 (const succ)
 
 testGr :: Graph
 testGr = buildG (0,8) [ (i,j) | (i,ns) <- es, j <- ns ]
@@ -25,9 +35,25 @@ testGr = buildG (0,8) [ (i,j) | (i,ns) <- es, j <- ns ]
          , (8, [2]     )
          ]
 
+distances :: Map.Map Int Int
+distances = Map.fromList [ (0,0)
+                         , (1,1)
+                         , (2,2)
+                         , (3,1)
+                         , (4,3)
+                         , (5,3)
+                         , (6,1)
+                         , (7,2)
+                         , (8,3)
+                         ]
+
+
 answer :: Tree Int
-answer = Node {rootLabel = 0,
-               subForest = [Node {rootLabel = 1
-                                 , subForest = [Node {rootLabel = 2
-                                                     , subForest = [Node {rootLabel = 5
-                                                                         , subForest = []}]}]},Node {rootLabel = 3, subForest = []},Node {rootLabel = 6, subForest = [Node {rootLabel = 7, subForest = [Node {rootLabel = 8, subForest = []},Node {rootLabel = 4, subForest = []}]}]}]}
+answer = Node 0 [ Node 6 [Node 7 [ Node 4 []
+                                 , Node 8 []
+                                 ]
+                         ]
+                , Node 3 [ Node 2 [ Node 5 [] ]
+                         ]
+                , Node 1  []
+                ]
