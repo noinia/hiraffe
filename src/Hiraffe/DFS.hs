504df42c61a9f21fa9fe7658b1b7927b07d55dc0
Frank Staals <frank@fstaals.net>
2024-12-26 18:59:30 +0100

    dfs on a more functional graph representation

--- src/Hiraffe/DFS.hs
+++ src/Hiraffe/DFS.hs
@@ -11,6 +11,9 @@
 module Hiraffe.DFS
   ( dfs
   , dff
+
+  , dfs'
+  , dff'
   ) where
 
 import           Control.Lens
@@ -23,18 +26,16 @@ import           Hiraffe.Graph
 import           Witherable
 
 --------------------------------------------------------------------------------
+-- * DFS and Dff on an actual graph
 
 -- | Depth first Search (DFS) to discover all components in the graph.
 --
 -- pre: - the fromEnum instance of a vertex maps the vertices to the range [0,..,V-1]
 --
 -- \(O(V+E)\)
-dff   :: (Graph_ graph, Enum (VertexIx graph), Ord (EdgeIx graph))
+dff   :: (Graph_ graph, Enum (VertexIx graph))
       => graph -> [TrieF (KV []) (EdgeIx graph) (VertexIx graph)]
-dff g = runST $ do
-          bv <- intitialize (numVertices g)
-          flip witherM (g^..vertices.asIndex) $ \u ->
-            dfsFrom g u bv
+dff g = dff' (\u -> g^..neighboursOfByEdge u.asIndex) (numVertices g) (g^..vertices.asIndex)
 
 -- | DFS, from a given vertex.
 --
@@ -42,31 +43,95 @@ dff g = runST $ do
 --      - the fromEnum instance of a vertex maps the vertices to the range [0,..,V-1]
 --
 -- \(O(V+E)\)
-dfs         :: (Graph_ graph, Enum (VertexIx graph), Ord (EdgeIx graph))
-            => graph -> VertexIx graph -> TrieF (KV []) (EdgeIx graph) (VertexIx graph)
-dfs g start = runST $ do
-                bv     <- intitialize (numVertices g)
-                -- start will be unvisited, as per precondition, thus the fromJust is safe
-                fromJust <$> dfsFrom g start bv
+dfs   :: (Graph_ graph, Enum (VertexIx graph))
+      => graph -> VertexIx graph -> TrieF (KV []) (EdgeIx graph) (VertexIx graph)
+dfs g = dfs' (\u -> g^..neighboursOfByEdge u.asIndex) (numVertices g)
+
+--------------------------------------------------------------------------------
+-- * DFS and DFf on an implicit graph (given by a function).
+
+-- | Depth first Search (DFS) to discover all components in the graph.
+--
+-- pre: - the fromEnum instance of a vertex maps the vertices to the range [0,..,V-1]
+--
+-- \(O(V+E)\)
+dff'               :: (Witherable f, Witherable g, Enum vertexIx)
+                   => (vertexIx -> g (edge, vertexIx))
+                      -- ^ Function to generate the out neighbours
+                   -> Int
+                      -- ^ the number of vertices
+                   -> f vertexIx
+                      -- ^ the vertices
+                   -> f (TrieF (KV g) edge vertexIx)
+dff' g n vertices' = runST $ do
+    bv <- intitialize n
+    flip witherM vertices' $ \u ->
+      dfsFrom g u bv
+
+-- | DFS from the given vertex.
+-- The graph is represented by a function that specifies the out vertices.
+--
+-- pre: - the vertex actually occurs in the graph!
+--      - the fromEnum instance of a vertex maps the vertices to the range [0,..,V-1]
+dfs'                          :: ( Witherable g, Enum vertexIx)
+                              => (vertexIx -> g (edge, vertexIx))
+                              -- ^ Function to generate the out neighbours
+                              -> Int
+                              -- ^ the number of vertices
+                              -> vertexIx
+                              -- ^ start vertex
+                              -> TrieF (KV g) edge vertexIx
+dfs' outNeighboursOf' n start = runST $ do
+    bv     <- intitialize n
+    -- start will be unvisited, as per precondition, thus the fromJust is safe
+    fromJust <$> dfsFrom outNeighboursOf' start bv
+
+--------------------------------------------------------------------------------
+-- * The implementation
+
+-- -- | Runs the DFS from a given start node with the given vertex
+-- dfsFrom            :: (Graph_ graph, Enum (VertexIx graph), Ord (EdgeIx graph))
+--                    => graph
+--                    -> VertexIx graph
+--                    -> UMV.MVector s Bool
+--                    -> ST s (Maybe (TrieF (KV []) (EdgeIx graph) (VertexIx graph)))
+-- dfsFrom g
+-- start bv = go start
+--   where
+--     go u = visited u >>= \case
+--       True  -> pure Nothing
+--       False -> do
+--                   visit u
+--                   Just . Node u . KV <$> witherM go' (g^..neighboursOfByEdge u.asIndex)
+
+--     go' (e,v) = fmap (e,) <$> go v
+
+--     visit   u = UMV.write bv (fromEnum u) True
+--     visited u = UMV.read  bv (fromEnum u)
 
 -- | Runs the DFS from a given start node with the given vertex
-dfsFrom            :: (Graph_ graph, Enum (VertexIx graph), Ord (EdgeIx graph))
-                   => graph
-                   -> VertexIx graph
-                   -> UMV.MVector s Bool
-                   -> ST s (Maybe (TrieF (KV []) (EdgeIx graph) (VertexIx graph)))
-dfsFrom g start bv = go start
+dfsFrom                           :: (Enum vertexIx, Witherable g)
+                                  => (vertexIx -> g (edge, vertexIx) )
+                                     -- ^ Function to generate the out neighbours
+                                  -> vertexIx
+                                  -> UMV.MVector s Bool
+                                  -> ST s (Maybe (TrieF (KV g) edge vertexIx))
+dfsFrom outNeighboursOf' start bv = go start
   where
     go u = visited u >>= \case
       True  -> pure Nothing
       False -> do
                   visit u
-                  Just . Node u . KV <$> witherM go' (g^..neighboursOfByEdge u.asIndex)
+                  Just . Node u . KV <$> witherM go' (outNeighboursOf' u)
 
     go' (e,v) = fmap (e,) <$> go v
 
     visit   u = UMV.write bv (fromEnum u) True
     visited u = UMV.read  bv (fromEnum u)
+
+
+
+
 
 -- | Initialize the vector with visited marks
 intitialize   :: Int -> ST s (UMV.MVector s Bool)
